9a9cce0ea76de017af20f2871d40a17f9fc33733
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2024-08-28 20:49:37 -0300

    inicio de projeto, configurações iniciais

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center w-full bg-red-400">
+      <h1 className="font-extrabold"> Ola mundo!</h1>
     </div>
   );
 }
