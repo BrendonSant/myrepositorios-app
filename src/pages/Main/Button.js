95a22cfa31493ae69d06328b97227071df0edcff
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2024-08-29 12:23:33 +0000

    Atualizações no input

--- src/pages/Main/Button.js
+++ src/pages/Main/Button.js
@@ -1,9 +1,13 @@
-import React from "react";
+
 import { FaPlus } from 'react-icons/fa';  // Você importou o ícone FaPlus corretamente?
 
+
 export default function Button() {
+
+   
+
     return (
-        <button>
+        <button type="submit" >
             <div className=" bg-black   ml-1 p-2 rounded ">
                 <FaPlus  color="white" size={14} />
             </div>
--- src/pages/Main/Form.js
+++ src/pages/Main/Form.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { FaGithub } from 'react-icons/fa';
+
+import React, {useState} from "react";
+
 import Button from "./Button";  // Certifique-se de que o caminho está correto
 
 export default function Form() {
+
+    const [newRepo, setNewRepo] = useState('');
+
+    function handleinputChange(e){
+        setNewRepo(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        console.log(newRepo);
+    }
+
     return (
-        <form className=" flex justify-center">
-            <input className="  h-7 rounded border border-gray-400 "  type="text" placeholder=" Adicionar repositorios" />
+        <form className=" flex justify-center" onSubmit={handleSubmit}>
+            <input className="  h-7 rounded border border-gray-400 "  type="text" 
+            placeholder=" Adicionar repositorios"
+            value={newRepo}
+            onChange={handleinputChange}
+             />
             <Button />
         </form>
     );
